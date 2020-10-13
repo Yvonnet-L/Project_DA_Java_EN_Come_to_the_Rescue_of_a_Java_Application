700a0f4a42066988b86265b0b09e3b00c84f3ee0
Laurent Yvonnet <yvonnet_l@yahoo.fr>
2020-10-13 09:53:32 +0200

    Création de la class CountSymptoms qui retournera la liste triée par ordre alphabéthique

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/SortSymptoms.java
@@ -0,0 +1,24 @@
+package com.hemebiotech.analytics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+/**
+ *  Sort the list send then return it
+ * @author ptiqu
+ *
+ */
+public class SortSymptoms {
+
+	public static List<String> lst = new ArrayList<>();
+/**
+ * 
+ * @param lst represents the list of uncalculated symptoms
+ * @return ranked list
+ */
+	public static List<String> sortlst(List<String> lst) {
+	Collections.sort(lst);
+	return lst;
+	}
+
+}

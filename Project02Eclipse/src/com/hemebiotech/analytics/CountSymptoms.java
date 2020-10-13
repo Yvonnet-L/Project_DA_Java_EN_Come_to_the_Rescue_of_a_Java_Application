3f751077ca5f18089d36b5f367174c99b60b6583
Laurent Yvonnet <yvonnet_l@yahoo.fr>
2020-10-13 09:48:34 +0200

    Création de la class CountSymptoms qui retournera la liste triée par ordre alphabéthique

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/CountSymptoms.java
@@ -0,0 +1,46 @@
+package com.hemebiotech.analytics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+/**
+ *  Receipt of a list to return the list of words with their number of occurrences
+ * @author ptiqu
+ *
+ */
+public class CountSymptoms {
+	
+
+	public static Map<String, Integer> mlst = new HashMap<>();
+	public static List<String> resultL = new ArrayList<>();
+
+	public CountSymptoms (List<String> resultL){
+		this.resultL = resultL;
+	}
+	/**
+	 *   Receipt of a list to return the list of words with their number of occurrences
+	 * @param resultL : the list accessed in alphabetical order
+	 * @return we return a Treemap :  the symptoms with their number of occurrences
+	 */
+	public static Map<String, Integer> Count(List<String> resultL) {
+	
+		Map<String, Integer> occurrences = new TreeMap<>();
+		for(String st : resultL)
+		{
+			if( occurrences.containsKey(st))
+			{
+			// If it exists, we increment the cumulation which represents the number of occurrences //
+				int cumul = occurrences.get(st);
+				cumul +=1;
+				occurrences.put(st, cumul);
+			//  Otherwise we create the symptom line //
+			} else { 
+				occurrences.put(st, 1);
+				}
+		}
+		//System.out.println(occurrences.toString());
+		return occurrences;	
+	}
+}
